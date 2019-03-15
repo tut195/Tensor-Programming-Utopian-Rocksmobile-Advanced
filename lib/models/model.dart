d037020a07de41bf038b59410c8ebbfeeaad977b
Vladimir Babenko <babenkovladimirbmd@gmail.com>
2019-03-15 09:47:37 +0200

    models;

--- lib/models/model.dart
+++ lib/models/model.dart
@@ -39,6 +39,16 @@ class Contribution {
         status = json['status'];
 }
 
+class GithubModel {
+  final String tagName;
+  final String htmlUrl;
+
+  GithubModel(this.tagName, this.htmlUrl);
+
+  GithubModel.fromJson(Map json)
+      : this.tagName = json['tag_name'],
+        this.htmlUrl = json['html_url'];
+}
 // {
 //     "author": "froq",
 //     "beneficiaries_set": true,
